b6fe2d91e38a553270f784744b8df61a73cce2c7
Alberto Ferroni <alberto.ferroni@4codesrl.it>
2021-11-07 17:57:24 +0100

    Assets

--- lib/src/presentation/utils/category.dart
+++ lib/src/presentation/utils/category.dart
@@ -1,42 +1,42 @@
 List<Category> category = [
   Category(
     name: "Ordinary Drink",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/ordinary.svg",
     filter: "Ordinary Drink",
   ),
   Category(
     name: "Cocktail",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/cockt.svg",
     filter: "Cocktail",
   ),
   Category(
     name: "Milk \/ Float \/ Shake",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/milk.svg",
     filter: "Milk \/ Float \/ Shake",
   ),
   Category(
     name: "Other\/Unknown",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/unknown.svg",
     filter: "Other\/Unknown",
   ),
   Category(
     name: "Cocoa",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/cocoa.svg",
     filter: "Cocoa",
   ),
   Category(
     name: "Shot",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/shot.svg",
     filter: "Shot",
   ),
   Category(
     name: "Coffee \/ Tea",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/coffee.svg",
     filter: "Coffee \/ Tea",
   ),
   Category(
     name: "Homemade Liqueur",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/homemade.svg",
     filter: "Homemade Liqueur",
   ),
   Category(
@@ -46,12 +46,12 @@ List<Category> category = [
   ),
   Category(
     name: "Beer",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/beer.svg",
     filter: "Beer",
   ),
   Category(
     name: "Soft Drink \/ Soda",
-    asset: "assets/images/drink.svg",
+    asset: "assets/images/soda.svg",
     filter: "Soft Drink \/ Soda",
   ),
 ];
