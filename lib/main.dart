24670e9e6d662b2cd1bb4ce9628549c7d4f256fc
Albenga96 <albertoferroni.af@gmail.com>
2021-11-08 15:28:47 +0100

    Added only potrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,6 @@ import 'src/cocktails_app.dart';
 
 void main() {
   configureDependencies();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const CocktailsApp());
 }
--- lib/src/cocktails_app.dart
+++ lib/src/cocktails_app.dart
@@ -5,6 +5,7 @@ import 'package:cocktailapp/src/presentation/providers/cocktails.dart';
 import 'package:cocktailapp/src/presentation/views/auth_view.dart';
 import 'package:cocktailapp/src/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +14,10 @@ class CocktailsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
